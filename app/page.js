ed52add1cbc83ae71f29eaf4da7148f22a5971d6
sergiopmdev <sergio.pm.dev@proton.me>
2023-08-03 17:10:45 +0000

    [MAIN] Implement shadcn/ui
    Implementation of the shadcn/ui module to manage the UI of the application

--- app/page.js
+++ app/page.js
@@ -1,3 +1,5 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <h1 className="text-4xl">Home page</h1>;
+  return <Button variant="outline">Home page</Button>;
 }
